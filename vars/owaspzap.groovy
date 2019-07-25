1e543773dde8babbb75307c6d91d891c03ec7bfa
tmr <temmar.abdessamad@gmail.com>
2019-07-26 00:19:21 +0200

    edit

--- vars/owaspzap.groovy
+++ vars/owaspzap.groovy
@@ -23,7 +23,7 @@ podTemplate(
         ),
         containerTemplate(
             name: 'zap',
-            image: 'owasp/zap2docker-stable',
+            image: 'zap',
             ttyEnabled: true,
             alwaysPullImage: true,
             envVars: [
