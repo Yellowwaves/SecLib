be0756c35a2f2c1acafc6a1eb1426cdb293a459b
unknown <temmar.abdessamad@gmail.com>
2019-07-28 02:00:19 +0200

    edits

--- vars/owaspzap.groovy
+++ vars/owaspzap.groovy
@@ -53,7 +53,7 @@ podTemplate(
                         echo "DefectDojo API KEY     : $DOJO_API_KEY"
 			
 			sh "ls -ltr /reports"
-            sh "mkdir -p /zap/wrk; zap-baseline.py -t ${config.target_url}"
+            sh "mkdir -p /zap/wrk; zap-baseline.py -t ${config.target_url} || true"
             sh "ls -ltr /reports"
                     }
                 }
