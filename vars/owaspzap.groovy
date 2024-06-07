11f227e85dc19f8744bbbd565e71f20586588ec9
Elouan Teissere <71369993+Yellowwaves@users.noreply.github.com>
2024-06-08 00:26:45 +0800

    Delete defectdojo

--- vars/owaspzap.groovy
+++ vars/owaspzap.groovy
@@ -16,61 +16,56 @@ def call(Closure configBlock) {
     configBlock()
     def uniqId = UUID.randomUUID().toString()
 
-podTemplate(
-    label: "jnlpslave-zap-${config.engagement_id}",
-    containers: [
-        containerTemplate(
-            name: 'jnlp',
-            image: 'jenkinsci/jnlp-slave',
-            ttyEnabled: true,
-            privileged: true,
-            envVars: [
-            containerEnvVar(
-                key: 'HOME',
-                value: '/home/jenkins/'
-                ),
-            ]
-        ),
-        containerTemplate(
-            name: 'zap',
-            image: 'abdessamadtmr/zap',
-            ttyEnabled: true,
-            privileged: true,
-            alwaysPullImage: true
-        )
-    ],
-  volumes: [
-    hostPathVolume(mountPath: "/var/run/docker.sock", hostPath: "/var/run/docker.sock"),
-    persistentVolumeClaim(mountPath: '/zap/reports', claimName: 'reports-data', readOnly: false)
-  ]
-) {
-    node("jnlpslave-zap-${config.engagement_id}"){
-        try {
-            stage('Checkout') {
-                checkout scm
-            }
-            stage('ZAP Analysis') {
-                container('zap'){
-                    withCredentials([string(credentialsId: 'dojo_url', variable: 'DOJO_URL'), string(credentialsId: 'dojo_api_key', variable: 'DOJO_API_KEY')]) {
+    podTemplate(
+        label: "jnlpslave-zap-${config.engagement_id}",
+        containers: [
+            containerTemplate(
+                name: 'jnlp',
+                image: 'jenkinsci/jnlp-slave',
+                ttyEnabled: true,
+                privileged: true,
+                envVars: [
+                    containerEnvVar(
+                        key: 'HOME',
+                        value: '/home/jenkins/'
+                    ),
+                ]
+            ),
+            containerTemplate(
+                name: 'zap',
+                image: 'abdessamadtmr/zap',
+                ttyEnabled: true,
+                privileged: true,
+                alwaysPullImage: true
+            )
+        ],
+        volumes: [
+            hostPathVolume(mountPath: "/var/run/docker.sock", hostPath: "/var/run/docker.sock"),
+            persistentVolumeClaim(mountPath: '/zap/reports', claimName: 'reports-data', readOnly: false)
+        ]
+    ) {
+        node("jnlpslave-zap-${config.engagement_id}"){
+            try {
+                stage('Checkout') {
+                    checkout scm
+                }
+                stage('ZAP Analysis') {
+                    container('zap'){
                         echo "Engagement Id          : ${config.engagement_id}"
                         echo "Target URL             : ${config.target_url}"
-                        echo "DefectDojo URL         : $DOJO_URL"
-                        echo "DefectDojo API KEY     : $DOJO_API_KEY"
-			
-            sh "zap-baseline.py -t ${config.target_url} -A $DOJO_API_KEY -I ${config.engagement_id} -U $DOJO_URL"
+
+                        // Exécuter votre analyse ZAP ici
+                        sh "zap-baseline.py -t ${config.target_url}"
                     }
                 }
             }
-        }
-        catch(Exception e) {
-            println "error message : ${e}"
-        }
-        finally {
-            Notification{
-                notification_to = config.notification_to ?: false
+            catch(Exception e) {
+                println "error message : ${e}"
+            }
+            finally {
+                // Vous pouvez ajouter d'autres actions ici
             }
         }
     }
-}
 }
 
